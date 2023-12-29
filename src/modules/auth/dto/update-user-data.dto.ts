b897c775a905d38ea514a2037960fdd1e8e27e06
Andres <andres99.dev@gmail.com>
2023-12-29 21:07:02 +0100

    fix 500 error

--- src/modules/auth/dto/update-user-data.dto.ts
+++ src/modules/auth/dto/update-user-data.dto.ts
@@ -1,14 +1,12 @@
-import { ApiProperty, PickType, PartialType } from '@nestjs/swagger'
+import { PartialType, PickType } from '@nestjs/swagger'
 import { IsOptional } from 'class-validator'
 
 import { User } from '../entities/auth.entity'
 
 export class UpdateUserDataDto extends PartialType(PickType(User, ['name', 'avatar'] as const)) {
   @IsOptional()
-  @ApiProperty({ required: false })
   avatar?: string
 
   @IsOptional()
-  @ApiProperty({ required: false })
   name?: string
 }
